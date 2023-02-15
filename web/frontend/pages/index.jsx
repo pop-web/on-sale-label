07b8932b4831b34acb84cd9c6a20c65a151e689f
koga yusuke <popweb782@gmail.com>
2023-02-16 01:23:10 +0900

    rename

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -17,7 +17,7 @@ import { ProductsCard } from "../components";
 export default function HomePage() {
   return (
     <Page narrowWidth>
-      <TitleBar title="App name" primaryAction={null} />
+      <TitleBar title="セールスラベル" primaryAction={null} />
       <Layout>
         <Layout.Section>
           <Card sectioned>
